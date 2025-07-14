b160bf2b1c1c61752134669069c49448ac673f89
Satak Kumar Dey <130965098+satak100@users.noreply.github.com>
2025-07-14 17:53:30 +0600

    Delete backend directory

--- backend/hashpass.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const plainPassword = 'architectpass'; // Replace with your desired password
-
-bcrypt.hash(plainPassword, saltRounds, (err, hash) => {
-    if (err) throw err;
-    console.log('Hashed Password:', hash);
-});
--- backend/src/config/db.js
+++
@@ -1,9 +0,0 @@
-// src/config/db.js
-const { Pool } = require('pg');
-require('dotenv').config();
-
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-});
-
-module.exports = pool;
